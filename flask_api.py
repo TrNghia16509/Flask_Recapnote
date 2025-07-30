44dd7b43cad18bbff38ea32ff21870d94840479e
TrNghia16509 <101879831+TrNghia16509@users.noreply.github.com>
2025-07-30 22:53:20 +0700

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -36,4 +36,5 @@ def upload_audio():
         })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Railway sẽ set PORT
+    flask_app.run(host="0.0.0.0", port=port)
