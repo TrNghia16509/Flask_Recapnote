279ce28abced4d48c6780e4d74f2be29403c428f
TrNghia16509 <101879831+TrNghia16509@users.noreply.github.com>
2025-08-06 20:23:05 +0700

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -43,5 +43,6 @@ def upload_audio():
         })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
